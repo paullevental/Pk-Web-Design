10ad7c328f74e8f1b52641e686957dd644d8456f
Paul Levental <paul.levental@gmail.com>
2024-05-30 12:32:14 -0700

    First addition | unordered list for Navigation Bar / Header

---
+++ src/NavBar.tsx
@@ -0,0 +1,12 @@
+
+function header() {
+    return (
+        <ul>
+            <li><h1>Home</h1></li>
+            <li><h1>Work</h1></li>
+            <li><h1>Contact Us</h1></li>
+        </ul>
+    );
+}
+
+export default header;
